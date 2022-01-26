d1ac0fe3f03d9f5a83f59b39e49c09a176840a55
Mary <mariyapiligrim3@gmail.com>
2022-01-26 16:25:30 +0200

    Add HowItWorks component

---
+++ client/src/pages/HowItWorks/HowItWorks.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import HowItWorksComponent from '../../components/HowItWorksComponent/HowItWorksComponent';
+
+const HowItWorks = () => {
+  return (
+    <div>
+      <div>
+        <HowItWorksComponent/>
+      </div>
+
+    </div>
+  );
+}
+
+export default HowItWorks;
