8800104b7ef32d18b0d2273161de8e555c494397
Mary <mariyapiligrim3@gmail.com>
2022-01-26 16:26:05 +0200

    Add basic structure

---
+++ client/src/components/HowItWorksComponent/HowItWorksComponent.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Title from './Title/Title';
+
+const HowItWorksComponent = () => {
+  return (
+    <div>
+      <div>
+       <Title/>
+      </div>
+    </div>
+  );
+}
+
+export default HowItWorksComponent;
