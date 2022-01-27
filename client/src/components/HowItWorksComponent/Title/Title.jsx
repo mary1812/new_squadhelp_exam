a9a842f8fb3c1019baea79991bbbe86848382048
Mary <mariyapiligrim3@gmail.com>
2022-01-27 21:49:44 +0200

    Add classname for section

--- client/src/components/HowItWorksComponent/Title/Title.jsx
+++ client/src/components/HowItWorksComponent/Title/Title.jsx
@@ -7,15 +7,16 @@ import CONSTANTS from '../../../constants';
 const Title = () => {
   return (
     <div>
-      <div className={styles.sectionContainer}>
-        <section>
+      <div className={styles.titleContainer}>
+        <section className={styles.textSection}>
         <small>World's #1 Naming Platform</small>
         <h1>How Does Squadhelp Work?</h1>
         <p>Squadhelp helps you come up with a great name for your business by combining the power of crowdsourcing with sophisticated technology and Agency-level validation services.</p>
         <a className="text"> <span className="playVideo" ></span>  Play Video</a>
         </section>
-        <section>
-        <img src={`${CONSTANTS.STATIC_IMAGES_PATH}titleChange.png`} alt="Smartphone" />
+
+        <section className={styles.imgSection}>
+        <img src={`${CONSTANTS.STATIC_IMAGES_PATH}titlePhone.png`} alt="Smartphone" />
         </section>
       </div>
     </div>
