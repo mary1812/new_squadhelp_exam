6c362396eb7fea2a016f6e008d082de7b20969ac
Mary <mariyapiligrim3@gmail.com>
2022-05-04 22:59:59 +0300

    Add container wrapper

--- client/src/components/HowItWorksComponent/HowItWorksComponent.jsx
+++ client/src/components/HowItWorksComponent/HowItWorksComponent.jsx
@@ -1,22 +1,26 @@
-import React from 'react';
-import Title from './Title/Title';
-import ServicesSection from './ServicesSection/ServicesSection';
-import styles from './HowItWorksComponent.module.sass'
-import ContestsWork from './ContestsWork/ContestsWork';
-import Questions from './Questions/Questions';
+import React from "react";
+import Title from "./Title/Title";
+import ServicesSection from "./ServicesSection/ServicesSection";
+import styles from "./HowItWorksComponent.module.sass";
+import ContestsWork from "./ContestsWork/ContestsWork";
+import Questions from "./Questions/Questions";
+import StatInformation from "./StatInformation/StatInformation";
+import { Container } from "react-bootstrap";
 
 const HowItWorksComponent = () => {
   return (
-    <div className={styles.mainContainer}>
-       <Title />
-       <ServicesSection />
-      <ContestsWork/>
-      <Questions/>
-    </div>
+    <Container>
+      <div className={styles.mainContainer}>
+        <Title />
+        <ServicesSection />
+        <ContestsWork />
+        <Questions />
+        <StatInformation />
+      </div>
+    </Container>
   );
-}
+};
 
 export default HowItWorksComponent;
-
 
 // className={styles.mainContainer}
