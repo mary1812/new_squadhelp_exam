10979582b8a30f091024e7ef433aefc7b47e7102
Mary <mariyapiligrim3@gmail.com>
2022-05-07 01:03:54 +0300

    Add new classnames

--- client/src/components/HowItWorksComponent/QuestionBox/QuestionBox.jsx
+++ client/src/components/HowItWorksComponent/QuestionBox/QuestionBox.jsx
@@ -1,40 +1,36 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { Button, Collapse } from "react-bootstrap";
-import styles from "./QuestionBox.module.sass"
+import styles from "./QuestionBox.module.sass";
 
 const QuestionBox = ({ dataForQuestionBox, index }) => {
   const [open, setOpen] = useState(false);
 
   useEffect(() => {
-    if(index === 0){
-      setOpen(!open)
+    if (index === 0) {
+      setOpen(!open);
     }
   }, []);
 
   return (
-    <div style={{ margin: "0px 0px 16px" }}>
-      <Button
-        style={{
-          color: "#1E2022",
-          backgroundColor: "transparent",
-          border: "1px solid #e7eaf3",
-          display: "flex",
-          padding: "16px",
-          width: "100%",
-        }}
-        onClick={() => setOpen(!open)}
-        aria-controls="example-collapse-text"
-        aria-expanded={open}
-      >
-        {" "}
-        {dataForQuestionBox.question}{" "}
-      </Button>
-      <Collapse
-        style={{ padding: "16px", border: "1px solid #e7eaf3" }}
-        in={open}
-      >
-        <div id="example-collapse-text">{dataForQuestionBox.answer}</div>
-      </Collapse>
+    <div className={styles.qstnBox}>
+      <div style={{ margin: "0px 0px 16px"}}>
+        <Button
+         className="d-flex justify-content-between"
+          onClick={() => setOpen(!open)}
+          aria-controls="example-collapse-text"
+          aria-expanded={open}
+        >
+          {" "}
+          {dataForQuestionBox.question}{" "}
+          <span className="fas fa-arrow-down small"></span>
+        </Button>
+        <Collapse
+          style={{ padding: "16px", border: "1px solid #e7eaf3" }}
+          in={open}
+        >
+          <div id="example-collapse-text">{dataForQuestionBox.answer}</div>
+        </Collapse>
+      </div>
     </div>
   );
 };
--- client/src/components/HowItWorksComponent/Questions/Questions.jsx
+++ client/src/components/HowItWorksComponent/Questions/Questions.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import QuestionContainer from "../QuestionContainer/QuestionContainer";
 import CONSTANTS from "../../../constants";
+import { ListGroup } from "react-bootstrap";
+import styles from "./Questions.module.sass"
 
 const Questions = () => {
   const dataForQuestionsLaunching = CONSTANTS.QUESTION_ITEMS_LAUNCHING;
@@ -9,16 +11,19 @@ const Questions = () => {
   const dataForQuestionsCreatives = CONSTANTS.QUESTION_ITEMS_CREATIVES;
 
   return (
-    <div className="container row justify-content-lg-between align-items-start">
-      <nav className="js-sticky-block card border-0 bg-primary col col-3 ">
-        <div className="js-scroll-nav list-group list-group-transparent list-group-white">
+    
+    <div className="container row justify-content-lg-around align-items-start ">
+      <ul className="js-sticky-block card bg-primary col-3" >
+        <div className={styles.listGroup}>
+        <div className="js-scroll-nav list-group">
           <a className="btn btn-primary py-3 btn-sm text-start" href="/">Launching A Contest</a>
           <a  className="btn btn-primary py-3 btn-sm text-start" href="/">Buying From Marketplace</a>
           <a  className="btn btn-primary py-3 btn-sm text-start" href="/">Managed Contest</a>
           <a  className="btn btn-primary py-3 btn-sm text-start" href="/">For Creatives</a>
         </div>
-      </nav>
-
+      </div>
+      </ul>
+      
       <div className="col col-lg-8">
         <QuestionContainer
           header={"Launching A Contest"}
