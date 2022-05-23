5ada4a20758d0d8e1846bfa4ad30c8ce71d9836e
mary1812 <mariyapiligrim3@gmail.com>
2022-05-23 23:53:07 +0300

    feat: add GetStarted component

---
+++ client/src/components/HowItWorksComponent/GetStarted/GetStarted.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Button } from "react-bootstrap";
+import styles from "./GetStarted.module.sass";
+
+const GetStarted = () => {
+  return (
+    <div className={styles.font}>
+      <div className={styles.gradientPrimary}>
+        <div className={styles.textInfo}>
+          <h3 className="h2 text-info font-weight-semi-bold mb-2">
+            Ready to get started?
+          </h3>
+          <p className="lead text-white mb-3 fw-normal">
+            Fill out your contest brief and begin receiving custom name
+            suggestions within minutes.
+          </p>
+          <Button variant="outline-*">Start A Contest</Button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default GetStarted;
