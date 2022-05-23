f214b7272a890aa98a70e0b0eeb299315709f5af
mary1812 <mariyapiligrim3@gmail.com>
2022-05-23 23:53:33 +0300

    feat: add StatInformation component

--- client/src/components/HowItWorksComponent/StatInformation/StatInformation.jsx
+++ client/src/components/HowItWorksComponent/StatInformation/StatInformation.jsx
@@ -1,64 +1,93 @@
 import React from "react";
 import styles from "./StatInformation.module.sass";
-import { Button } from "react-bootstrap";
 import CONSTANTS from "../../../constants";
-import { Row, Col } from "react-bootstrap";
+
 const StatInformation = () => {
   return (
     <div>
-      <div className={styles.gradientPrimary}>
-        <div className={styles.textInfo}>
-          <h3 className="h2 text-info font-weight-semi-bold mb-2">
-            Ready to get started?
-          </h3>
-          <p className="lead text-white mb-3 fw-normal">
-            Fill out your contest brief and begin receiving custom name
-            suggestions within minutes.
-          </p>
-          <Button variant="outline-*">Start A Contest</Button>
-        </div>
-      </div>
       <div>
-        <Row className="justify-content-md-center text-center">
-          <Col>
-            <img
-              className="mx-auto"
-              style={{ height: "100px", width: "100px" }}
-              src={`${CONSTANTS.STATIC_IMAGES_PATH}starInfo.png`}
-            ></img>
-            <p className="mb-0">
-              <span className="text-dark fw-bold">4.9 out of 5 stars</span> from
-              25,000+ customers.
-            </p>
-          </Col>
-          <Col>
-            <img
-              className="mx-auto"
-              style={{ height: "100px", width: "100px" }}
-              src={`${CONSTANTS.STATIC_IMAGES_PATH}peopleInfo.png`}
-            ></img>
-            <p className="mb-0">
-              Our branding community stands{" "}
-              <span className="text-dark fw-bold">200,000+</span> strong.
-            </p>
-          </Col>
-          <Col>
-            <img
-              className="mx-auto"
-              style={{ height: "100px", width: "100px" }}
-              src={`${CONSTANTS.STATIC_IMAGES_PATH}documentInfo.png`}
-            ></img>
-            <p className="mb-0">
-              <span className="text-dark fw-bold">140+ Industries</span>{" "}
-              supported across more than{" "}
-              <span className="text-dark fw-bold">85 countries </span> - and
-              counting.
-            </p>
-          </Col>
-        </Row>
+        <div className="row justify-content-lg-center">
+          <div className="col-md-4 mb-7 mb-lg-0">
+            <div className="u-indicator-vertical-line text-center px-5">
+              <img
+                className="mx-auto mb-3"
+                style={{ height: "72px", width: "77px" }}
+                src={`${CONSTANTS.STATIC_IMAGES_PATH}stars.svg`}
+              ></img>
+              <p className="mb-0">
+                <span className="text-dark fw-normal">4.9 out of 5 stars</span>{" "}
+                from 25,000+ customers.
+              </p>
+            </div>
+          </div>
+          <div className="col-md-4 mb-7 mb-lg-0">
+            <div className="u-indicator-vertical-line text-center px-5">
+              <img
+                className="mx-auto mb-3"
+                style={{ height: "72px", width: "77px" }}
+                src={`${CONSTANTS.STATIC_IMAGES_PATH}peoplesInfo.png`}
+              ></img>
+              <p className="mb-0">
+                Our branding community <br /> stands
+                <span className="text-dark fw-normal"> 200,000+</span> strong.
+              </p>
+            </div>
+          </div>
+          <div className="col-md-4 px-5">
+            <div className="text-center">
+              <img
+                className="mx-auto mb-3"
+                style={{ height: "72px", width: "77px" }}
+                src={`${CONSTANTS.STATIC_IMAGES_PATH}sharingFiles.svg`}
+              ></img>
+              <p className="mb-0">
+                <span className="text-dark fw-normal"> 140+ Industries </span>
+                 supported <br/> across more than
+                <span className="text-dark fw-normal"> 85 countries </span> <br/> -
+                and counting.
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
 };
 
 export default StatInformation;
+
+// <Col className="col-md-3 px-5">
+// <img
+//   className="mx-auto"
+//   style={{ height: "72px", width: "77px" }}
+//   src={`${CONSTANTS.STATIC_IMAGES_PATH}stars.svg`}
+// ></img>
+// <p className="mb-0">
+//   <span className="text-dark fw-normal">4.9 out of 5 stars</span> from
+//   25,000+ customers.
+// </p>
+// </Col>
+// <Col className="col-md-3 px-5">
+// <img
+//   className="mx-auto"
+//   style={{ height: "72px", width: "77px" }}
+//   src={`${CONSTANTS.STATIC_IMAGES_PATH}peoplesInfo.png`}
+// ></img>
+// <p className="mb-0">
+//   Our branding community stands{" "}
+//   <span className="text-dark fw-normal">200,000+</span> strong.
+// </p>
+// </Col>
+// <Col className="col-md-3 px-5" >
+// <img
+//   className="mx-auto"
+//   style={{ height: "72px", width: "77px" }}
+//   src={`${CONSTANTS.STATIC_IMAGES_PATH}sharingFiles.svg`}
+// ></img>
+// <p className="mb-0">
+//   <span className="text-dark fw-normal">140+ Industries</span>{" "}
+//   supported across more than{" "}
+//   <span className="text-dark fw-normal">85 countries </span> - and
+//   counting.
+// </p>
+// </Col>
