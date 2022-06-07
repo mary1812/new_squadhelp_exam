585372fb8f1b956b0d4a416fa8ea6c0fea7ae880
mary1812 <mariyapiligrim3@gmail.com>
2022-06-08 02:58:30 +0300

    Implemented a timer inside the progressbar

--- client/src/components/ListOfTimers/ListOfTimers.jsx
+++ client/src/components/ListOfTimers/ListOfTimers.jsx
@@ -1,14 +1,13 @@
 import React, { useState, useContext, useRef, useEffect } from "react";
 import { Field } from "formik";
+import { Grid } from "@mui/material";
 import { TimerContext } from "../../contexts";
-import "./ListOfTimers";
-import ProgressBar from "../ProgressBar/ProgressBar";
+import "./ListOfTimers.css";
 import CountdownTimer from "../CountdownTimer/CountdownTimer";
-import CreateNewTimer from "../CreateNewTimer/CreateNewTimer";
+import TimerProgressBar from "../TimerProgressBar/TimerProgressBar";
 
 const ListOfTimers = () => {
   const [events, setEvents] = useContext(TimerContext);
-
 
   console.log(events);
   if (events.length > 0) {
@@ -17,20 +16,52 @@ const ListOfTimers = () => {
 
   const eventsArray = events.map((event) => {
     return (
-      <section key={event.eventName}>
-        <p>Имя события:</p>{event.eventName}
-        <p>Дата окончания события:</p>{event.eventEndDate}
-        <p>Дата уведомления о событии:</p>{event.eventNotification}
-        <CountdownTimer countdownDate={event.eventEndDate}/>
-        
+      <section key={event.eventName} className="listOfEvents">
+        {/* <p>Имя события:</p>
+        {event.eventName} */}
+        {/* <p>Дата окончания события:</p>
+        {event.eventEndDate}
+        <p>Дата уведомления о событии:</p>
+        {event.eventNotification} */}
+        <div className="wrapperBtnProgress">
+          <div>
+        <Grid container spacing={1} justify="space-between">
+          <Grid item xs={12} spacing={0}>
+            <div>
+              <span className="progressLabel">{event.eventName}</span>
+            </div>
+            <TimerProgressBar
+              progressDate={event.eventEndDate}
+              createDate={event.eventCreateDate}
+            />
+            <CountdownTimer countdownDate={event.eventEndDate} />
+          </Grid>
+            </Grid>
+            </div>
+        <div className="btnDeleteDone">
+          <button className="faBtn">
+            <i class="far fa-trash-alt"></i>
+          </button>
+          <button className="faBtn">
+            <i class="far fa-check-circle"></i>
+          </button>
+          </div>
+          </div>
       </section>
     );
   });
   return (
     <div className="listTimersContainer">
-      <ol>
-        {eventsArray}
-      </ol>
+      <div className="headerOfListTimers">
+        <div className="upcommingChecks">
+          <h1>Live upcomming checks</h1>
+        </div>
+        <div className="remainingTime">
+          <p style={{ paddingRight: "5px" }}>Remaining time</p>
+          <i class="far fa-clock"></i>
+        </div>
+      </div>
+      <ol>{eventsArray}</ol>
     </div>
   );
 };
