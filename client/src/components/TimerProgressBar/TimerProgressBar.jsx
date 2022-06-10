bdfc7d37367262d99ead45251f30b5889d332cb7
mary1812 <mariyapiligrim3@gmail.com>
2022-06-10 16:39:11 +0300

    Fix not moving progress bag

--- client/src/components/TimerProgressBar/TimerProgressBar.jsx
+++ client/src/components/TimerProgressBar/TimerProgressBar.jsx
@@ -5,13 +5,50 @@ import LinearProgress from "@mui/material/LinearProgress";
 
 export default function TimerProgressBar(props) {
   const nowDate = Date.now();
-  const msTime = Date.parse(props.createDate)
+  const msTime = Date.parse(props.createDate);
   const objProg = Date.parse(props.progressDate);
-  const dateInPercent =
-    ((Date.now() - props.createDate) / (objProg - props.createDate)) * 100;
-  const [progress, setProgress] = React.useState(dateInPercent);
-  console.log(progress);
-  console.log(msTime)
+  function dateInPercent() {
+    return (
+      ((Date.now() - props.createDate) / (objProg - props.createDate)) * 100
+    );
+  }
+  const [progress, setProgress] = React.useState(dateInPercent());
+  // setProgress(dateInPercent)
+
+  // React.useEffect(() => {
+  //   const timer = setInterval(() => {
+  //     setProgress((oldProgress) => {
+  //       if (oldProgress === 100) {
+  //         return 0;
+  //       }
+  //       const diff = Math.random() * 10;
+  //       return Math.min(oldProgress + diff, 100);
+  //     });
+  //   }, 500);
+
+  //   return () => {
+  //     clearInterval(timer);
+  //   };
+  // }, []);
+
+  React.useEffect(() => {
+    const timer = setInterval(() => {
+      console.log(progress);
+      console.log(msTime);
+      
+      setProgress((oldProgress) =>
+        oldProgress >= 100
+          ? 0
+          : dateInPercent()
+      );
+    }, 1000);
+
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, [setProgress]);
+
   return (
     <div>
       <LinearProgress
