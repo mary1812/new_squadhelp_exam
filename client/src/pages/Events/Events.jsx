dece377aa6d9ccdbd9cace646a171d5dd4892e5d
mary1812 <mariyapiligrim3@gmail.com>
2022-06-15 22:17:12 +0300

    Add header & sorting events

--- client/src/pages/Events/Events.jsx
+++ client/src/pages/Events/Events.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useMemo } from "react";
+import Header from "../../components/Header/Header"
 import "./Events.css";
 import { TimerContext } from "../../contexts";
 import ListOfTimers from "../../components/ListOfTimers/ListOfTimers";
@@ -6,10 +7,14 @@ import CreateNewTimer from "../../components/CreateNewTimer/CreateNewTimer";
 import NotificationCircle from "../../components/NotificationCircle/NotificationCircle";
 const Events = () => {
   const [events, setEvents] = useState([]);
+  const compireDates = (event1, event2) => {
+    return Date.parse(event1.eventEndDate) - Date.parse(event2.eventEndDate)
+  }
 
   useMemo(() => {
-    if (window.localStorage.getItem('events') !== null) {      
-      setEvents(JSON.parse(window.localStorage.events))
+    if (window.localStorage.getItem('events') !== null) {
+      const eventsArray = JSON.parse(window.localStorage.events)
+      setEvents(eventsArray.sort(compireDates))
     } else {
       setEvents([])
     }
@@ -17,10 +22,10 @@ const Events = () => {
 
   return (
     <TimerContext.Provider value={[events, setEvents]}>
+      <Header/>
       <div className="eventsContainer">
         <div className="listOfTimers"><ListOfTimers /></div>
         <div className="createNewTimer"><CreateNewTimer /></div>
-        <NotificationCircle/>
       </div>
     </TimerContext.Provider>
   );
