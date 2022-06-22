29b5639c290e24746eabcb9820d3a21d2772c765
mary1812 <mariyapiligrim3@gmail.com>
2022-06-23 01:09:10 +0300

    Add active class for selected button

--- client/src/components/ButtonComponent/ButtonComponent.jsx
+++ client/src/components/ButtonComponent/ButtonComponent.jsx
@@ -1,24 +1,25 @@
-import React, { useState } from "react";
+import React from "react";
 import * as bootstrap from "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./ButtonComponent.css";
 
 export default function ButtonComponent(props) {
-  const [color, setColor] = useState(true);
+
   return (
-    <div
-      class={`card card-frame text-center mb-5 inActive ${
-        color ? "notActive" : "getActive"
-      }`}
-      onClick={() => setColor(!color)}
-    >
-      <div class="p-3 card-body text-center borderRad">
-        <span class="mb-3 mx-auto badge passiveColor badgeYesNo">
-          {props.badge}
-        </span>
-        <h5 class="mb-1 text-secondary small formtip textInBadge">{props.headerBadge} </h5>
+        <div
+          class={`card card-frame text-center mb-5 inActive ${
+            props.isActive ?"getActive" :"notActive"
+          }`}
+        >
+          <div class="card-body text-center textInCard ">
+            <span class="mb-3 mx-auto badge passiveColor badgeYesNo">
+              {props.badge}
+            </span>
+            <h5 class="mb-1 text-secondary small formtip textInBadge">
+              {props.headerBadge}{" "}
+            </h5>
+          </div>
       </div>
-    </div>
+
   );
 }
-
--- client/src/components/ButtonGroupComponent/ButtonGroupComponent.jsx
+++ client/src/components/ButtonGroupComponent/ButtonGroupComponent.jsx
@@ -1,26 +1,41 @@
-import React from "react";
-import "./ButtonGroupComponent.css"
+import React, { useState } from "react";
+import * as bootstrap from "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./ButtonGroupComponent.css";
 import ButtonComponent from "../ButtonComponent/ButtonComponent";
 
 export default function ButtonGroupComponent(badge, headerBadge) {
+  const [selectedId, setSelectedId] = useState('');
+
   const contents = [
     {
+      id:1,
       badge: "Yes",
       headerBadge: "The Domain should exactly match the name",
+
     },
     {
+      id:2,
       badge: "Yes",
       headerBadge: "But minor variations are allowed (Recommended)",
     },
     {
+      id:3,
       badge: "No",
       headerBadge: "I am only looking for a name, not a Domain",
     },
   ];
 
-  const badgeComponents = contents.map((content) => 
-    <ButtonComponent badge={content.badge} headerBadge={content.headerBadge} />
-  );
+  const buttonsGroup = contents.map((content) => (
+    <div onClick={() => {
+      setSelectedId(content.id)
+    }}>
+    <ButtonComponent
+      badge={content.badge}
+      headerBadge={content.headerBadge}
+      isActive={selectedId === content.id}
+    /></div>
+  ));
 
-  return <div className="btnGroupComponent">{badgeComponents} </div>;
+  return <div class="btnGrp">{buttonsGroup}</div>;
 }
