c18ad10bc70e2bc3242fd37ff82263b1e416995d
mary1812 <mariyapiligrim3@gmail.com>
2022-07-10 21:45:48 +0300

    Add a check for error status

--- server/src/handlerError/handler.js
+++ server/src/handlerError/handler.js
@@ -5,24 +5,32 @@ module.exports = (err, req, res, next) => {
   console.log(err);
 
   if (err instanceof TokenExpiredError) {
-    err.status = 419
+    if (!err.status) {
+      err.status = 419
+    }
     logger.error(err);
     return res.status(419).send('Token has expired');
   }
 
   if (err instanceof JsonWebTokenError) {
-    err.status = 401
+    if (!err.status) {
+      err.status = 401
+    }
     logger.error(err);
     return res.status(401).send('Bad token');
   }
 
   if (!err.message || !err.status) {
-    err.status = 500
+    if (!err.status) {
+      err.status = 500
+    }
     logger.error(err);
     return res.status(500).send('Server Error');
   }
   
-  err.status = 400
+  if (!err.status) {
+    err.status = 400
+  }
   logger.error(err);
   return res.status(err.status).send(err.message);
 };
