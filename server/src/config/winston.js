cef9e79f521d99f4138bcf9633417a81ac4c7cb1
mary1812 <mariyapiligrim3@gmail.com>
2022-07-12 00:13:25 +0300

    Add ability to copy content from server.log

--- server/src/config/winston.js
+++ server/src/config/winston.js
@@ -3,16 +3,17 @@ const { printf, combine, timestamp } = format;
 const {WINSTON_LOGS_PATH}  = require('./../constants');
 
 const logFormat = printf(({ status, message, timestamp, stack }) => {
-  return `{message: "${message}", time: ${new Date(timestamp).getTime()}, code: ${status}, stackTrace: {${stack}}}`;
+  return `{message: "${message}", time: ${new Date(timestamp).getTime()}, code: ${status}, stackTrace: {${stack}}};`;
 });
 
 const logger = createLogger({
   format: combine(timestamp(),
     format.errors({stack: true}),
-    logFormat),
+    logFormat
+    ),
   transports: [
     new transports.File({
-      filename: WINSTON_LOGS_PATH,
+      filename: WINSTON_LOGS_PATH
     }),
   ],
 });
--- server/src/index.js
+++ server/src/index.js
@@ -1,24 +1,43 @@
-require('dotenv').config();
-const http = require('http');
-const express = require('express');
-const cors = require('cors');
-require('./dbMongo/mongoose');
-const router = require('./router');
-const controller = require('./socketInit');
-const handlerError = require('./handlerError/handler');
-const CONSTANTS = require('./constants');
+require("dotenv").config();
+const http = require("http");
+const express = require("express");
+const cors = require("cors");
+require("./dbMongo/mongoose");
+const router = require("./router");
+const controller = require("./socketInit");
+const handlerError = require("./handlerError/handler");
+const CONSTANTS = require("./constants");
+const fs = require("fs");
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use('/public', express.static(CONSTANTS.FILES_PATH));
+app.use("/public", express.static(CONSTANTS.FILES_PATH));
 app.use(router);
 app.use(handlerError);
 
-const server = http.createServer(app);
-server.listen(PORT,
-  () => console.log(`Example app listening on port ${ PORT }!`));
-controller.createConnection(server);
+setInterval(() => {
+  const fileName = `./logs/${Date.now()}.log`;
+  fs.copyFile("./logs/server.log", fileName, function (err) {
+    if (err) {
+      console.log("Error", err);
+    } else {
+      const data = fs.readFileSync(fileName, "utf-8");
+      const dataArray = data.split(";");
+      
+      const outputArray = dataArray.map((obj) => {
+        const parsedLog = JSON.parse(obj)
+        return `{message: ${parsedLog.message}, code: ${parsedLog.code}, time: ${parsedLog.time}}`;
+      });
+      fs.writeFile(fileName, JSON.stringify(outputArray), console.log("Done"))
+    }
+  });
+}, 50000);
 
+const server = http.createServer(app);
+server.listen(PORT, () =>
+  console.log(`Example app listening on port ${PORT}!`)
+);
+controller.createConnection(server);
