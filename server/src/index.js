a6b3a846966ee690f13340fd01a33738197cc6a3
mary1812 <mariyapiligrim3@gmail.com>
2022-07-16 15:25:22 +0300

    Add decomposition & new structure for logs

--- server/src/index.js
+++ server/src/index.js
@@ -7,7 +7,7 @@ const router = require("./router");
 const controller = require("./socketInit");
 const handlerError = require("./handlerError/handler");
 const CONSTANTS = require("./constants");
-const fs = require("fs");
+const dailyReport = require("./../logger/dailyReport")
 
 const PORT = process.env.PORT || 3000;
 const app = express();
@@ -18,47 +18,7 @@ app.use("/public", express.static(CONSTANTS.FILES_PATH));
 app.use(router);
 app.use(handlerError);
 
-setInterval(() => {
-  const fileName = `./logs/${Date.now()}.log`;
-  fs.copyFile("./logs/server.log", fileName, function (err) {
-    if (err) {
-      console.log("Error", err);
-    } else {
-      fs.readFile(fileName, "utf-8", function (error, data) {
-        if (error) {
-          console.log(error);
-        }
-        const dataArray = data.split(";");
-        dataArray.pop();
-        const outputArray = dataArray.map((logString) => {
-          console.log(logString);
-          const parsedLog = JSON.parse(logString);
-          delete parsedLog.stackTrace;
-          return parsedLog;
-        });
-
-        fs.writeFile(
-          fileName,
-          JSON.stringify(outputArray, null, 2),
-          function (err, data) {
-            if (!err) {
-              console.log("Done");
-              fs.writeFile("./logs/server.log", "", function (err, data) {
-                if (!err) {
-                  console.log("Cleared");
-                } else {
-                  console.log(err);
-                }
-              });
-            } else {
-              console.log(err);
-            }
-          }
-        );
-      });
-    }
-  });
-}, 50000);
+dailyReport ();
 
 const server = http.createServer(app);
 server.listen(PORT, () =>
