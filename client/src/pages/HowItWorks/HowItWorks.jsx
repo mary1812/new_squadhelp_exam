a1c4059eaeeb23e0f5c34b90974c1cb53fb93c2d
mary1812 <mariyapiligrim3@gmail.com>
2022-07-26 20:41:40 +0300

    Restrict access for moderator

--- client/src/pages/HowItWorks/HowItWorks.jsx
+++ client/src/pages/HowItWorks/HowItWorks.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import HowItWorksComponent from '../../components/HowItWorksComponent/HowItWorksComponent';
 
-const HowItWorks = () => {
+const HowItWorks = (props) => {
+  if (props && props.data && props.data.role && props.data.role === 'moderator') {
+    props.history.replace("/moderation")
+  }
   return (
     <div>
       <div>
         <HowItWorksComponent/>
       </div>
-
     </div>
   );
 }
 
-export default HowItWorks;
+const mapStateToProps = (state) => {
+  const { data } = state.auth;
+  return { data };
+};
+
+export default connect(mapStateToProps, null)(HowItWorks);
+
