e3135d9b86d7211538e0ac48c0654edd2492f288
mary1812 <mariyapiligrim3@gmail.com>
2022-07-27 00:44:16 +0300

    Add ability to logout for moderator

--- client/src/pages/ModeratorPage/ModeratorPage.js
+++ client/src/pages/ModeratorPage/ModeratorPage.js
@@ -1,5 +1,37 @@
 import React from "react";
+import { connect } from 'react-redux';
+import { Link, withRouter } from 'react-router-dom';
+import CONSTANTS from "../../constants";
+import "./ModeratorPage.css";
+import { clearauth, headerRequest } from '../../actions/actionCreator';
 
-export default function ModeratorPage() {
-  return <div>ModeratorPage</div>;
+
+const ModeratorPage = (props) => {
+
+  const logOutModer = () => {
+    props.clearauth();
+    props.history.replace('/login');
+  };
+
+  return (
+    <div className="containerModerPage">
+      <img
+        src={`${CONSTANTS.STATIC_IMAGES_PATH}blue-logo.png`}
+        className="logoModerPage"
+      />
+      Moderator Page
+      <Link style={{textDecoration: 'none'}}>
+      <span onClick={logOutModer}>Logout</span>
+      </Link>
+    </div>
+  );
 }
+
+const mapStateToProps = (state) => state.auth;
+const mapDispatchToProps = (dispatch) => ({
+  getUser: () => dispatch(headerRequest()),
+  clearauth: () => dispatch(clearauth()),
+});
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(ModeratorPage));
+
