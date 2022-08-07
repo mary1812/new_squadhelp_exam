6076d61d2090aedc425c2186a38e858e9343084f
mary1812 <mariyapiligrim3@gmail.com>
2022-08-07 22:33:48 +0300

    Fix bag about updating page

--- client/src/components/PrivateRoute/PrivateRoute.js
+++ client/src/components/PrivateRoute/PrivateRoute.js
@@ -2,21 +2,19 @@ import React from "react";
 import { Route, Redirect } from "react-router-dom";
 import { useSelector } from "react-redux";
 import SpinnerLoader from "../Spinner/Spinner";
+import Home from "../../pages/Home/Home";
+import CONSTANTS from "../../constants";
 
 const PrivateRoute = ({ roles, ...routeConfig }) => {
-  const { isFetching, data } = useSelector((state) => state.auth);
-
-  if (isFetching) {
-    return <SpinnerLoader />;
-  }
-
-  if (data) {
-    if ((roles && roles.includes(data.role)) || !roles) {
-      return <Route {...routeConfig} />;
-    }
-  }
-
-  return <Redirect to="/" />;
+  const { data } = useSelector((state) => state.auth);
+  
+  return (roles && data && roles.includes(data.role)) || !roles ? (
+    <Route {...routeConfig} />
+  ) : window.localStorage.getItem(CONSTANTS.REFRESH_TOKEN) && !data ? (
+     <SpinnerLoader />
+  ) : (
+    <Redirect to="/" />
+  );
 };
 
 export default PrivateRoute;
