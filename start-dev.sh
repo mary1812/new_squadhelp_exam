217ec41c7f5282cf4742e38e019c41aaeb3749cd
mary1812 <mariyapiligrim3@gmail.com>
2022-08-20 16:01:50 +0300

    Rollback start dev script

--- start-dev.sh
+++ start-dev.sh
@@ -8,7 +8,7 @@
 started_at=$(date +"%s")
 
 echo "-----> Provisioning containers"
-docker-compose --file docker-compose-dev.yaml up -d
+docker-compose --file docker-compose-dev.yaml up
 echo ""
 
 # Run Sequalize's migrations.
@@ -28,6 +28,3 @@ seconds=$(((ended_at - started_at) % 60))
 
 echo "-----> Done in ${minutes}m${seconds}s"
 
-echo "-----> Running application"
-docker-compose --file docker-compose-dev.yaml up
-echo ""
