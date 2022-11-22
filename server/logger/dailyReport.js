f3078d9ae69a57dd3d6b59093e1443c1c20ec6af
mary1812 <mariyapiligrim3@gmail.com>
2022-11-22 18:03:45 +0200

    Refactor logger

--- server/logger/dailyReport.js
+++ server/logger/dailyReport.js
@@ -3,48 +3,58 @@ const logger = require("./../src/config/winston");
 const CONSTANTS = require("../src/constants");
 
 const dailyReport = () => {
-    const fileName = `./logs/${Date.now()}.log`;
-    fs.copyFile(CONSTANTS.WINSTON_LOGS_PATH, fileName, function (err) {
+  const fileName = `./logs/${Date.now()}.log`;
+  
+  fs.copyFile(CONSTANTS.WINSTON_LOGS_PATH, fileName, function (err) {
       if (err) {
         err.status = 503;
         logger.error(err);
-      } else {
-        fs.readFile(fileName, "utf-8", function (error, data) {
-          if (error) {
-            error.status = 503;
-            logger.error(error);
-          } else {
-            const dataArray = data.split(";");
-            dataArray.pop();
-            const outputArray = dataArray.map((logString) => {
-              const parsedLog = JSON.parse(logString);
-              delete parsedLog.stackTrace;
-              return parsedLog;
-            });
-            fs.writeFile(
-              fileName,
-              JSON.stringify(outputArray, null, 2),
-              function (err, data) {
-                if (!err) {
-                  console.log("Done");
-                  fs.writeFile(CONSTANTS.WINSTON_LOGS_PATH, "", function (err, data) {
-                    if (!err) {
-                      console.log("Cleared");
-                    } else {
-                      err.status = 503;
-                      logger.error(err);
-                    }
-                  });
-                } else {
-                  err.status = 503;
-                  logger.error(err);
-                }
-              }
-            );
-          }
-        });
+      }
+      else {
+        readLogsStream(fileName)
       }
   });
 };
+
+const readLogsStream = (fileName) => {
+  fs.readFile(fileName, "utf-8", function (error, data) {
+    if (error) {
+      error.status = 503;
+      logger.error(error);
+    } else {
+      const dataArray = data.split(";");
+      dataArray.pop();
+      const outputArray = dataArray.map((logString) => {
+        const parsedLog = JSON.parse(logString);
+        delete parsedLog.stackTrace;
+        return parsedLog;
+      });
+      writeLogsToFile(fileName, JSON.stringify(outputArray, null, 2))  
+    } 
+  });
+}
+
+const writeLogsToFile = (fileName, logsStream) => {
+  fs.writeFile(
+    fileName,
+    logsStream,
+    function (err) {
+      if (!err) {
+        console.log("Done");
+        fs.writeFile(CONSTANTS.WINSTON_LOGS_PATH, "", function (err) {
+          if (!err) {
+            console.log("Cleared");
+          } else {
+            err.status = 503;
+            logger.error(err);
+          }
+        });
+      } else {
+        err.status = 503;
+        logger.error(err);
+      }
+    }
+  );
+}
 
 module.exports = dailyReport;
